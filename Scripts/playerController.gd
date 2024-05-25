12aaf6e54e3c77d18644b2a6f78b3ab8c67a08a5
Brendon Stahl <brendonstahl97@gmail.com>
2024-05-25 18:51:08 -0400

    Added Bump Mechanic

--- Scripts/playerController.gd
+++ Scripts/playerController.gd
@@ -1,5 +1,7 @@
 extends RigidBody3D
 
+const FOOD_EXPLOSION = preload("res://Scenes/food_explosion.tscn")
+
 @onready var SprintEmitter1: CPUParticles3D = $"Particle Emitters/CPUParticles3D"
 @onready var SprintEmitter2: CPUParticles3D = $"Particle Emitters/CPUParticles3D2"
 @onready var animation_tree : AnimationTree = $Casual3_Male/AnimationTree
@@ -8,9 +10,14 @@ extends RigidBody3D
 @export var VelocityPower = float(1)
 @export var StandardAccelerationMultiplier = float(1)
 @export var DeccelerationMultiplier = float(1)
+@export var Controls: PlayerControls
 
 @export var MaxSprintModifier = 3
-@export var SprintIncrementAmount = float(0.1)
+@export var SprintIncrementAmount = float(0.1) # Per Second
+
+@export var BumpMomentumThreshold = 10 # Momentum
+@export var BumpDirectionThreshold = 30 # Degrees
+@export var BumpMultiplier = 1
 
 var MovementDirection = Vector3.ZERO
 var SprintModifier = float(1)
@@ -21,12 +28,12 @@ func _ready() -> void:
 	animation_tree.active = true
 
 
-func _process(delta: float) -> void:
+func _process(_delta: float) -> void:
 	update_animation_parameters()	
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _physics_process(delta: float) -> void:
-	MovementDirection = Vector3(Input.get_axis("move_left", "move_right"), 0, Input.get_axis("move_up", "move_down")).normalized()
+	MovementDirection = Vector3(Input.get_axis(Controls.move_left, Controls.move_right), 0, Input.get_axis(Controls.move_up, Controls.move_down)).normalized()
 	
 	_handle_sprint(delta)
 	_handle_movement()
@@ -53,17 +60,17 @@ func _handle_movement() -> void:
 	
 	
 func _handle_rotation() -> void:
-	if (linear_velocity.length() > 0.1):
+	if (linear_velocity.length() > 0.3 && MovementDirection.length() >= 0.3):
 		look_at_from_position(global_position, global_position + linear_velocity.normalized())
 		
 func _handle_sprint(delta: float) -> void: 
-	if (Input.is_action_just_pressed("Sprint")):
+	if (Input.is_action_just_pressed(Controls.sprint)):
 		_start_sprint_particles()
 		
-	elif (Input.is_action_pressed("Sprint") && SprintModifier < MaxSprintModifier):
+	elif (Input.is_action_pressed(Controls.sprint) && SprintModifier < MaxSprintModifier):
 		SprintModifier += SprintIncrementAmount * delta
 		
-	elif (Input.is_action_just_released("Sprint")):
+	elif (Input.is_action_just_released(Controls.sprint)):
 		SprintModifier = 1
 		_end_sprint_particles()
 		
@@ -77,9 +84,31 @@ func _end_sprint_particles() -> void:
 	SprintEmitter1.emitting = false
 	SprintEmitter2.emitting = false
 
-
 func update_animation_parameters():
 	animation_tree["parameters/idle_to_walk/blend_position"] = linear_velocity.length()
 
 
-
+func _on_body_entered(body: RigidBody3D) -> void:
+	if (body.get_groups().any(func(group): group != "Players")):
+		return
+		
+	var momentum = linear_velocity.length() * mass
+	print("momentum: ", momentum)
+	
+	if (momentum < BumpMomentumThreshold):
+		return
+		
+	var directionToBody = body.global_position - position
+	var bumpTrueness = rad_to_deg(directionToBody.angle_to(linear_velocity))
+	print("bumpTrueness: ", bumpTrueness)
+		
+	if (bumpTrueness > BumpDirectionThreshold):
+		return
+	
+	body.apply_impulse(directionToBody * momentum * BumpMultiplier)
+	body.apply_torque_impulse(Vector3.UP * momentum * BumpMultiplier * randf_range(-1, 1))
+	linear_velocity = Vector3.ZERO
+	
+	var foodExplosion = FOOD_EXPLOSION.instantiate()
+	foodExplosion.position = body.global_position
+	get_window().add_child(foodExplosion)
