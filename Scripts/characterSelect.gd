d4281c610407f3a08591bb57a3d55ec06fe0c0b6
Matthew Warlick Wilson <matwil98@yahoo.com>
2024-05-26 17:53:08 -0400

    Char Select and Game Environment

---
+++ Scripts/characterSelect.gd
@@ -0,0 +1,17 @@
+extends Node2D
+# Info to establish in this scene:
+# For each Player:
+#	No. of Players
+#	Stand Type
+#	Character Model
+#	Player Color
+#	Player Name
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
