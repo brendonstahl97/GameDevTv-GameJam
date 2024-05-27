9c82a9718436a94e246778c05d86071e3b2c2837
Brendon Stahl <brendonstahl97@gmail.com>
2024-05-27 00:15:25 -0400

    Helicopter Camera

--- Scripts/CameraManager.gd
+++ Scripts/CameraManager.gd
@@ -2,20 +2,60 @@ extends Camera3D
 
 var Players
 @export var xPlayerLimits: Vector2 ## how far positive or negative a player can go in the X direction before they are no longer included in the camera position calculations (x = min, y = max)
+@export var yPlayerLimits: Vector2 ## how far positive or negative a player can go in the X direction before they are no longer included in the camera position calculations (x = min, y = max)
+@export var zPlayerLimits: Vector2 ## how far positive or negative a player can go in the Z direction before they are no longer included in the camera position calculations (x = min, y = max)
 @export var lerpSpeed = 1.0
+
+var StartingPosition: Vector3
+var StartingFov: int
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
+	StartingPosition = position
+	StartingFov = fov
 	Players = get_tree().get_nodes_in_group("Players")
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	var xTotal = 0
-	for player in Players:
-		var playerPosX = player.position.x
-		if (playerPosX >= xPlayerLimits.x && playerPosX <= xPlayerLimits.y):
-			xTotal += playerPosX
-	var avgX = xTotal/Players.size()
 	
-	position = position.lerp(Vector3(avgX, position.y, position.z), delta * lerpSpeed)
+	var positionTotal = Vector3.ZERO
+	var maxPos = Vector3.ZERO
+	var minPos = Vector3.ZERO
+	var numPlayersInCalc = 0
+	
+	for player in Players:
+		var playerPos = player.position
+		
+		if (playerPos.x >= xPlayerLimits.x && playerPos.x <= xPlayerLimits.y && playerPos.z >= zPlayerLimits.x && playerPos.z <= zPlayerLimits.y && playerPos.y >= yPlayerLimits.x && playerPos.y <= yPlayerLimits.y):
+			numPlayersInCalc += 1
+			positionTotal += playerPos
+			
+			if (playerPos.x > maxPos.x):
+				maxPos.x = playerPos.x
+				
+			if (playerPos.x < minPos.x):
+				minPos.x = playerPos.x
+				
+			if (playerPos.z > maxPos.z):
+				maxPos.z = playerPos.z
+				
+			if (playerPos.z < minPos.z):
+				minPos.z = playerPos.z
+			
+	var avgPosition = positionTotal/numPlayersInCalc
+	var avgPositionNoY = Vector3(avgPosition.x, 0, avgPosition.z)
+	var positionRangeNoY = Vector3(maxPos.x, 0, maxPos.y) - Vector3(minPos.x, 0, minPos.y)
+	
+	var targetPos = Vector3(avgPositionNoY.x, position.y, position.z)
+	position = position.lerp(targetPos, delta * lerpSpeed)
+	
+	var targetFov = StartingFov + positionRangeNoY.length()/2
+	fov = lerp(fov, targetFov, delta * lerpSpeed)
+	
+	var currentTransform = transform
+	look_at(avgPosition)
+	var lookAtTransform = transform
+	transform = currentTransform.interpolate_with(lookAtTransform, delta * lerpSpeed)
+	
+	
