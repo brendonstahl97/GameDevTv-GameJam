e24ee48851560f822f01eedc1c0b7bdeaf38fc78
Brendon Stahl <brendonstahl97@gmail.com>
2024-05-29 11:48:55 -0400

    Updated character select code

--- Scripts/P1Panel.gd
+++ Scripts/P1Panel.gd
@@ -2,49 +2,60 @@ extends Panel
 var selected
 var joined = false
 var currentIndex = 0
+var borderStyleBox
+@onready var navigable_items: Control = $NavigableItems
+var verticalItems
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	selected = get_children()[4].name ## initialize focus on the press a to join box
+	selected = get_child(2).name ## initialize focus on the press a to join box
 	get_children()[0].placeholder_text = "Player" + name ## set current player box to current player number
 	#make all ui elements invisible except the joiner box 
 	for child:Control in get_children():
 		if(child.name != "Joiner"):
 			child.visible = false
+			
+	borderStyleBox = navigable_items.get_child(0).get_theme_stylebox("panel").duplicate()
+	borderStyleBox.border_color = Color8(204,204,204,255)
+	borderStyleBox.border_width_bottom = 5
+	borderStyleBox.border_width_left = 5
+	borderStyleBox.border_width_top = 5
+	borderStyleBox.border_width_right = 5
+	
+	verticalItems = navigable_items.get_children()
 
-
-func _process(delta):
-	pass
-
-func _input(event):
-	print(currentIndex)	
+func _process(delta: float) -> void:
 	# when the a button is pressed make everything visible and make joiner box invisible
 	if( Input.is_action_just_pressed("p" + name + "_sprint") && !joined ):
-		joined = true
-		get_children()[4].visible = false
-		get_children()[0].visible = true
-		get_children()[1].visible = true
-		get_children()[2].visible = true
-		get_children()[3].visible = true
-		selected = get_children()[1].name ## switch focus to first user input element 
-		currentIndex = 1 ## I don't know how else to keep this number (current index in the getChildren array)
+		_join_player()
 	
-	# creates up and down ui movement and bind to first and last array indexes
-	var boxRange = get_child_count() - 2
-	if( joined && Input.is_action_just_pressed("p" + name + "_code_down")):
-		currentIndex = (currentIndex + boxRange) % boxRange
-		currentIndex+=1
-		selected = get_children()[currentIndex].name
-	if( joined && Input.is_action_just_pressed("p" + name + "_code_up")):
-		currentIndex = (currentIndex + boxRange + 1) % boxRange
-		currentIndex+=1
-		selected = get_children()[currentIndex].name
+	if (joined):
+		if(Input.is_action_just_pressed("p" + name + "_code_down")):
+			_navigate_vertically(1)
+		if(Input.is_action_just_pressed("p" + name + "_code_up")):
+			_navigate_vertically(-1)
 		
+		_handle_horizontal_navigation()
 
+func _join_player() -> void:
+	joined = true
+	get_child(0).visible = true
+	get_child(1).visible = true
+	get_child(2).visible = false
 	
-	for child:Control in get_children():
-		if( child.name.match("LineEdit") ):
-			continue
+	selected = verticalItems[0].name ## switch focus to first user input element 
+	currentIndex = 0 ## I don't know how else to keep this number (current index in the getChildren array)
+
+func _navigate_vertically(steps: int) -> void:
+	var boxRange = verticalItems.size()
+	print("Current Index: ", currentIndex, ", Box Range: ", boxRange, ", Steps:", steps)
+	currentIndex = (currentIndex + boxRange + steps) % boxRange
+	print("New Index: ", currentIndex)
+	selected = verticalItems[currentIndex].name
+	_apply_border_style(verticalItems[currentIndex])
+	
+func _handle_horizontal_navigation() -> void:
+	for child:Control in verticalItems:
 		if( (child.name.match(selected) ) && (Input.is_action_just_pressed("p" + name + "_code_left") || Input.is_action_just_pressed("p" + name + "_move_left") ) ):
 			if(!child.select_previous_available()):
 				child.current_tab = child.get_tab_count()-1
@@ -52,12 +63,12 @@ func _input(event):
 			if( !child.select_next_available() ):
 				child.current_tab = 0
 		if(child.name.match(selected)):
-			var new_stylebox = child.get_theme_stylebox("panel").duplicate()
-			new_stylebox.border_color = Color8(204,204,204,255)
-			new_stylebox.border_width_top = 5
-			new_stylebox.border_width_bottom = 5
-			new_stylebox.border_width_left = 5
-			new_stylebox.border_width_right = 5
-			child.add_theme_stylebox_override("panel", new_stylebox)
+			child.add_theme_stylebox_override("panel", borderStyleBox)
 		else:
 			child.remove_theme_stylebox_override("panel")
+
+func _apply_border_style(selectedItem: Node) -> void:
+		if(selectedItem.name.match(selected)):
+			selectedItem.add_theme_stylebox_override("tab_focus", borderStyleBox)
+		else:
+			selectedItem.remove_theme_stylebox_override("tab_focus")
