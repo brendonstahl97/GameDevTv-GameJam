c9afb701f25322a9d52d78068dd1dfdcc3f20dfd
CullyLine <Linebergercully@gmail.com>
2024-05-31 23:57:53 -0400

    end of game stuff

---
+++ Scripts/endOfGameScreen.gd
@@ -0,0 +1,25 @@
+extends Node
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	# Get the player info, and replace each player stuffs with the relevant info.
+	for playerKey in global.playerInfo:
+		# Set the UI (color, name and score)
+
+
+		# Set the guy
+
+
+		# Set the cart
+
+		pass
+
+	# Remove the un-used player stuffs
+
+	
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
