38accd661ade29fbef6fae9b99663195f06cd650
Brendon Stahl <brendonstahl97@gmail.com>
2024-06-02 13:56:20 -0400

    Samina bar bug fix

--- Scripts/Stamina Bar.gd
+++ Scripts/Stamina Bar.gd
@@ -7,9 +7,9 @@ var LastTargetValue
 
 func _process(delta: float) -> void:
 	StaminaProgressBar.value = lerp(StaminaProgressBar.value, LastTargetValue, delta * LerpSpeed)
-	if (StaminaProgressBar.value > 99.8):
+	if (StaminaProgressBar.value > 99.5):
 		StaminaProgressBar.value = 100
-	elif (StaminaProgressBar.value < 1.2):
+	elif (StaminaProgressBar.value < 1.5):
 		StaminaProgressBar.value = 1
 
 func _on_stamina_manager_stamina_changed(value: float) -> void:
