756d0ca0c7f4caa52a49de9544201f54974b115c
Brendon Stahl <brendonstahl97@gmail.com>
2024-06-02 11:23:23 -0400

    Added Spawn points to Killbox

--- Scenes/Killbox.gd
+++ Scenes/Killbox.gd
@@ -1,6 +1,5 @@
 extends Area3D
-
-@export var RespawnPoints: Array
+@onready var spawn_points: Node = $"../SpawnPoints"
 
 func _on_body_exited(body: Player) -> void:
 	if (!body is Player):
@@ -9,5 +8,14 @@ func _on_body_exited(body: Player) -> void:
 	if (!body.get_groups().has("Players")):
 		return
 
-	body.global_position = RespawnPoints.pick_random()
-	body.linear_velocity = Vector3.ZERO
+	Respawn(body)
+		
+		
+func Respawn(body: Player) -> void:
+	var selectedSpawn = spawn_points.get_children().pick_random()
+	
+	if (selectedSpawn is Node3D):
+		body.global_position = selectedSpawn.global_position
+		body.linear_velocity = Vector3.ZERO
+	else: 
+		Respawn(body)
