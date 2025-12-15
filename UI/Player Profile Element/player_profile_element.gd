077cb271976ac33ddb20e05d20d1b10a5886d80c
brendonstahl97 <brendonstahl97@gmail.com>
2025-12-15 13:57:11 -0500

    Added navigation arrows and delete indicator to profile element

--- UI/Player Profile Element/player_profile_element.gd
+++ UI/Player Profile Element/player_profile_element.gd
@@ -5,6 +5,7 @@ extends Control
 
 @onready var tab_container: PlayerProfileTabContainer = %"PlayerProfileTabContainer"
 @onready var navigation_arrows: NavigationArrows = %NavigationArrows
+@onready var delete_prompt: Control = %DeletePrompt
 
 var is_creating_profile: bool = false
 
@@ -25,10 +26,12 @@ func update(assigned_player_control_stack: ControlStack) -> void:
 
 func pseudo_focus() -> void:
 	navigation_arrows.show()
+	delete_prompt.show()
 
 
 func exit_pseudo_focus() -> void:
 	navigation_arrows.hide()
+	delete_prompt.hide()
 
 
 func get_selected_profile_id() -> int:
@@ -49,3 +52,13 @@ func previous_tab() -> void:
 
 func delete_current_profile() -> void:
 	tab_container.delete_current_profile()
+
+
+func _on_profile_creator_begin_create_profile() -> void:
+	if (delete_prompt.visible):
+		delete_prompt.hide()
+
+
+func _on_profile_creator_end_create_profile() -> void:
+	if (!delete_prompt.visible):
+		delete_prompt.show()
